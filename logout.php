d8cd2d078755939c1aebc3fb49d7faf7fefa8f1f
barahaw <barahawamdeh2003@gmail.com>
2025-06-07 17:43:55 +0300

    v 1.01

--- logout.php
+++ logout.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
+setcookie('success_message', 'تم تسجيل الخروج بنجاح.', time() + 60, '/');
+
 session_unset();
 session_destroy();
-
-$_SESSION['success_message'] = "تم تسجيل الخروج بنجاح.";
 
 header("Location: login.php");
 exit;
